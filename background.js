f6ad96e6c761861ed5e7942b348ffc813e9f3948
ttay265 <tuannq265@gmail.com>
2018-05-17 06:32:45 +0700

    as
    as

--- background.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.create({url: chrome.runtime.getURL("new_raid.html")});
-});
